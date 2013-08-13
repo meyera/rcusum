e261c1b498f56bf111cdfd419d29eba59ae1300b
Alexander Meyer <meyer.alxander+github@gmail.com>
2013-08-13 09:22:05 +0200

    Implemented some dynamic input controls

--- inst/rcusum-ShinyApp/server.R
+++ inst/rcusum-ShinyApp/server.R
@@ -1,19 +1,65 @@
 library(shiny)
+library(rcusum)
 
 # Define server logic required to generate and plot a random distribution
 shinyServer(function(input, output) {
   
-  # Expression that generates a plot of the distribution. The expression
-  # is wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should be automatically 
-  #     re-executed when inputs change
-  #  2) Its output type is a plot 
-  #
-  output$distPlot <- renderPlot({
+  datasetInput <- reactive({
     
-    # generate an rnorm distribution and plot it
-    dist <- rnorm(input$obs)
-    hist(dist)
+    # input$file1 will be NULL initially. After the user selects and uploads a 
+    # file, it will be a data frame with 'name', 'size', 'type', and 'datapath' 
+    # columns. The 'datapath' column will contain the local filenames where the 
+    # data can be found.
+    inFile <- input$file1
+    
+    if (is.null(inFile))
+      return(NULL)
+    
+    read.csv(inFile$datapath, header=input$header, sep=input$sep, quote=input$quote)
+  })
+  
+  output$contents <- renderTable({
+    datasetInput()
+  })
+  output$strContents <- renderPrint({
+    str(datasetInput())
+  })
+  
+  output$failure_indicator_chooser <- renderUI({
+    ind <- names(datasetInput())
+    if (is.null(ind) | length(ind) == 0) {
+      return(tags$div(style="color:red", "No data uploaded!", tags$br(),"Please upload data."))
+    } else {
+      return(selectInput("failure_indicator", "Choose Failure Indicator Variable", ind))
+    }
+  })
+  
+  output$by_chooser <- renderUI({
+    ind <- names(datasetInput())
+    if (is.null(ind) | length(ind) == 0) {
+      ind = c()
+    }
+    
+    return(selectInput("by_indicator", "Choose a stratification Variable", c("No stratification" ,ind)))
+  })
+  
+  output$unadjusted <- renderPlot({
+    df = datasetInput()
+    failures = df[input$failure_indicator]
+    by = NULL
+    if (!is.null(input$by_indicator) & input$by_indicator != "No stratification") {
+      by = df[input$by_indicator]
+    }
+    alpha = input$alpha
+    beta = input$beta
+    
+    p1 = cusum(failures, p0=p0, p1=p1, alpha=alpha, beta=beta, by=by, loglike_chart=FALSE)
+    p2 = cusum(failures, p0=p0, p1=p1, alpha=alpha, beta=beta, by=by, loglike_chart=TRUE)
+    p3 = cusum.obs_minus_exp(failures,p0=p0, by=by)
+  })
+  
+  output$risk_adjusted <- renderPlot({
+    p1 = cusum.sprt()
+    p3 = cusum.obs_minus_exp()
   })
 })
--- inst/rcusum-ShinyApp/ui.R
+++ inst/rcusum-ShinyApp/ui.R
@@ -4,19 +4,56 @@ library(shiny)
 shinyUI(pageWithSidebar(
   
   # Application title
-  headerPanel("Hello Shiny!"),
+  headerPanel("rcusum - An app for conducting interactive cusum analysis"),
   
   # Sidebar with a slider input for number of observations
   sidebarPanel(
-    sliderInput("obs", 
-                "Number of observations:", 
-                min = 1,
-                max = 1000, 
-                value = 500)
+    tags$h4("Data File Upload"),
+    fileInput('file1', 'Choose CSV File',
+              accept=c('text/csv', 'text/comma-separated-values,text/plain')),
+    checkboxInput('header', 'Header', TRUE),
+    radioButtons('sep', 'Separator',
+                 c(Comma=',',
+                   Semicolon=';',
+                   Tab='\t'),
+                 'Comma'),
+    radioButtons('quote', 'Quote',
+                 c(None='',
+                   'Double Quote'='"',
+                   'Single Quote'="'"),
+                 'Double Quote'),
+    tags$hr(),
+    tags$h4("General Cusum Settings"),
+    uiOutput("failure_indicator_chooser"),
+    uiOutput("by_chooser"),
+    tags$br(),
+    sliderInput("alpha", 
+                "Alpha-Level: Type I error rate", 
+                min = 0,
+                max = 0.5, 
+                value = 0.01,
+                step=0.01
+                ),
+    sliderInput("beta", 
+                "Beta-Level: Type II error rate", 
+                min = 0,
+                max = 0.5, 
+                value = 0.01,
+                step=0.01
+    )
   ),
   
   # Show a plot of the generated distribution
   mainPanel(
-    plotOutput("distPlot")
+    tabsetPanel(
+      tabPanel("Data", textOutput("strContents"),tableOutput('contents')),
+      tabPanel("Unadjusted Cusum Analysis",
+               plotOutput("unadjusted")
+               ),
+      tabPanel("Risk-Adjusted Cusum Analysis",
+               plotOutput("risk_adjusted")
+               ),
+      tabPanel("Help", tags$a(href="https://github.com/meyera/rcusum", "Go to the package development website for help"))
+    )
   )
 ))
